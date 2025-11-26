780dd091eecba9706e95691c3ac6b62dbc6cc19d
TheKings294 <98644094+TheKings294@users.noreply.github.com>
2025-11-26 21:54:00 +0100

    fix: edit navBar for reponsive

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,10 @@
 import {Link, NavLink} from "react-router";
-import {useState} from "react";
 import logo from '../assets/tic_tac_toe.webp'
 import {House, ChartNoAxesCombined, GamepadDirectional} from 'lucide-react'
 import {useLocalStorage} from "@/hooks/useLocalStorage.tsx";
 import type {SaverGame} from "@/types/BoardType.ts";
 
 function Navbar() {
-    const [open, setOpen] = useState(false);
     const [game] = useLocalStorage<SaverGame>('game', {
         username: "",
         player1Wins: 0,
@@ -23,23 +21,9 @@ function Navbar() {
                     <img src={logo} width={"60rem"} height={"60rem"} />
                 </Link>
 
-                <button
-                    onClick={() => {
-                        setOpen(!open)
-                    }}
-                    className="md:hidden"
-                    aria-label="Toggle Menu"
-                >
-                    <div className="w-6 h-0.5 mb-1 bg-current"></div>
-                    <div className="w-6 h-0.5 mb-1 bg-current"></div>
-                    <div className="w-6 h-0.5 bg-current"></div>
-                </button>
-
                 <div
-                    className={`absolute left-0 top-full w-full flex-col items-center gap-4 py-4 transition-all 
-                    md:static md:flex md:flex-row md:w-auto md:py-0 ${
-                        open ? "flex" : "hidden"
-                    }`}
+                    className={`flex-col items-center gap-4 py-4 transition-all 
+                    md:static flex md:flex-row md:w-auto md:py-0`}
                 >
                     <NavLink to="/" className={({ isActive, isPending }) => (
                             isActive ? "btn bg-orange px-3 py-2 rounded-xl border border-transparent transition-all " +
